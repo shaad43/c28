93852ece9d1d377c0c221202cacf92c461c2c5fd
shaad43 <77321217+shaad43@users.noreply.github.com>
2021-03-21 17:49:53 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,24 @@
+  
+class Stone  {
+    constructor(x,y,r){
+        var options={
+            isStatic:false,
+            restitution:1,
+            friction:1,
+            density:1.2
+        }
+        this.x = x
+        this.y = y
+        this.r = r
+        this.body = Bodies.circle(x,y,this.r/2,options);
+        World.add(world,this.body)
+    }
+    display(){
+        var pos = this.body.position;
+        push();
+        rotate(this.body.angle)
+        ellipseMode(CENTER);
+        ellipse(pos.x,pos.y,this.r);
+        pop();
+    }
+  }
